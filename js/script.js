9794e98907cb5d0ac6d6b41d82956c30b5ad6abf
Axmadiyor <rayimkulovaxmadiyor@gmail.com>
2023-07-14 17:35:44 +0500

    yangilandi

--- js/script.js
+++ js/script.js
@@ -2,3 +2,8 @@
 
 const channel = prompt("Whats your favourite youtuber?", "")
 console.log(`https://youtube.com/@${channel}`)
+ 
+const axmed = true
+const guli = false
+
+console.log(axmed || guli)
