2ebddf1d5129665307fa34b1a4cc2977c8aac3c0
Axmadiyor <rayimkulovaxmadiyor@gmail.com>
2023-07-14 17:19:46 +0500

    new

--- js/script.js
+++ js/script.js
@@ -2,11 +2,3 @@
 
 const channel = prompt("Whats your favourite youtuber?", "")
 console.log(`https://youtube.com/@${channel}`)
-
-const seyHello = 6+12
-console.log(typeof seyHello)
-
-const isAge = true
-const isClose = false
-
-console.log(isAge && isClose)
